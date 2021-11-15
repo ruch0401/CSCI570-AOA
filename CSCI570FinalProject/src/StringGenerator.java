2376c22b42f2e45dde22f2ce7bac81dec098d73a
ruch0401 <ruch0401@gmail.com>
2021-11-15 15:11:40 -0800

    Adding rudimentary logic to read data from file

--- CSCI570FinalProject/src/StringGenerator.java
+++ CSCI570FinalProject/src/StringGenerator.java
@@ -1,12 +1,36 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringGenerator {
     public static void main(String[] args) throws Exception {
-        String baseString1 = "ACTG";
-        int[] indexes1 = {3, 6, 1};
-        String baseString2 = "TACG";
-        int[] indexes2 = {1, 2, 9};
+        // Reading data from the file
+        Path path = Paths.get("CSCI570FinalProject/resources/input.txt");
+        List<String> data = Files.readAllLines(path);
+        System.out.println(data);
+        int blankStringIndex = data.indexOf("");
 
-        String finalString1 = generateInputString(baseString1, indexes1);
-        String finalString2 = generateInputString(baseString2, indexes2);
+        // Creating input for the generateString() method
+        // The bifurcation and index allotment has been done by adding a blank character between the 2 sets of inputs
+        String firstBaseString = data.get(0);
+        String secondBaseString = data.get(blankStringIndex + 1);
+        List<Integer> indexes1 = new ArrayList<>();
+        List<Integer> indexes2 = new ArrayList<>();
+
+        for (int i = 1; i < blankStringIndex; i++) {
+            indexes1.add(Integer.parseInt(data.get(i)));
+        }
+
+        for (int i = blankStringIndex + 2; i < data.size(); i++) {
+            indexes2.add(Integer.parseInt(data.get(i)));
+        }
+
+        String finalString1 = generateInputString(firstBaseString, indexes1);
+        String finalString2 = generateInputString(secondBaseString, indexes2);
         System.out.println(finalString1 + "\n" + finalString2);
     }
 
@@ -17,8 +41,8 @@ public class StringGenerator {
      * @return cumulated string
      * @throws Exception if the length of the generated string does not match with the supposed length of the cumulated string
      */
-    public static String generateInputString(String base, int[] indexes) throws Exception {
-        int lengthSupposedToBe =  (int) (Math.pow(2, indexes.length)) * base.length();
+    public static String generateInputString(String base, List<Integer> indexes) throws Exception {
+        int lengthSupposedToBe =  (int) (Math.pow(2, indexes.size())) * base.length();
         StringBuilder sb = null;
         for (int index: indexes) {
             sb = new StringBuilder(base);
